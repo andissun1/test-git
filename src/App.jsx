928c768618505c684c5c7ee429455ac1550aefd8
Андрей <Winterles@yandex.ru>
2025-06-04 15:35:09 +0300

    Задание 1. Добавление строки с текущим годом на JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+// Императивный стиль
 function App() {
   const [count, setCount] = useState(0);
 
@@ -24,6 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more.</p>
+      <p className="read-the-docs">Текущий год: {`${new Date().getFullYear}`} </p>
     </>
   );
 }
